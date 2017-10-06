4f44e6c374f5178cd888e8a30cb9c6ed05eaf8e3
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-10-06 12:38:00 +0330

    Minor changes in the comment form

--- SSC_Site/mezzacaptcha/forms.py
+++ SSC_Site/mezzacaptcha/forms.py
@@ -1,8 +1,9 @@
 from captcha.fields import ReCaptchaField
 from mezzanine.generic.forms import ThreadedCommentForm
+from django.utils.translation import ugettext_lazy as _
 
 
 class CaptchaThreadedCommentForm(ThreadedCommentForm):
     """ Adds a reCaptcha field to the comment form in blog
     """
-    captcha = ReCaptchaField()
+    captcha = ReCaptchaField(label=_('Captcha'))
