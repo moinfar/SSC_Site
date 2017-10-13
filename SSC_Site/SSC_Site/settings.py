9bf8e986bfd3e8f877072931c006f7d4efcca895
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-10-13 10:55:22 +0330

    increased max length of the slug url

--- SSC_Site/SSC_Site/settings.py
+++ SSC_Site/SSC_Site/settings.py
@@ -450,7 +450,7 @@ AUTHENTICATION_BACKENDS = (
 import string
 
 SHORTENER_SLUG_CHARSET = string.ascii_lowercase + string.digits
-SHORTENER_SLUG_LENGTH = 8
+SHORTENER_SLUG_LENGTH = 16
 
 ########################
 # Certificate Settings #
